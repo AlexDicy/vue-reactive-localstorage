5e9ef1ee08fd5484df2db9192573b384007337eb
Lorenzo Lapucci <halexercraft@gmail.com>
2023-05-09 18:55:20 +0200

    Update the test to include multiple nested objects and arrays

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,40 +1,39 @@
 <template>
-  <div id="app">
-      <h1>Test str and num</h1>
-    <input type="text" v-model="storage.str" placeholder="type value for str"><br><br>
-    <input type="number" v-model="storage.num" placeholder="type value for num">
-      <h1>Test obj and arr</h1>
-    <small>open Vue console to test below</small> <br><br>
-    <input type="text" v-model="newKeyValueObj.key" placeholder="type new key for obj"> <input type="text" v-model="newKeyValueObj.value" placeholder="new value for obj">
-    <button @click="addKeyValueToObj">add</button>
-    <br><br>
-    <input type="text" v-model="newValueToArr" placeholder="type new value for arr">
-    <button @click="addValueToArr">add</button>
-  </div>
+    <div id="app">
+        <h1>Test str and num</h1>
+        <input type="text" v-model="storage.str" placeholder="type value for str"><br><br>
+        <input type="number" v-model="storage.num" placeholder="type value for num">
+        <h1>Test obj and arr</h1>
+        <small>open Vue console to test below</small> <br><br>
+        <input type="text" v-model="newKeyValueObj.key" placeholder="type new key for obj"> <input type="text" v-model="newKeyValueObj.value" placeholder="new value for obj">
+        <button @click="addKeyValueToObj">add</button>
+        <br><br>
+        <input type="text" v-model="newValueToArr" placeholder="type new value for arr">
+        <button @click="addValueToArr">add</button>
+        <h2>Change thirdLevel.key3 value</h2>
+        <input type="text" v-model="storage.firstLevel.secondLevel.thirdLevel.key3" placeholder="type value for str"><br><br>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      newKeyValueObj: {
-          key: "",
-          value: ""
-      },
-      newValueToArr: ""
-    }
-},
-methods: {
-    addKeyValueToObj() {
-        // For reacticy with plains u must replace entire object becouse watch doesn't detect changes on further namespaces/vars inside it.
-        var copy = JSON.parse(JSON.stringify(this.storage.obj));
-            copy[this.newKeyValueObj.key] = this.newKeyValueObj.value;
-        this.storage.obj = copy;
+    name: 'app',
+    data () {
+        return {
+            newKeyValueObj: {
+                key: "",
+                value: ""
+            },
+            newValueToArr: ""
+        }
     },
-    addValueToArr() {
-        this.storage.arr.push(this.newValueToArr);
+    methods: {
+        addKeyValueToObj() {
+            this.storage[this.newKeyValueObj.key] = this.newKeyValueObj.value;
+        },
+        addValueToArr() {
+            this.storage.arr.push(this.newValueToArr);
+        }
     }
-}
 }
 </script>
--- test/src/main.js
+++ test/src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueReactiveStorage from "../../dist/index";
+import VueReactiveStorage from '../../dist/index';
+
 
 Vue.use(VueReactiveStorage, {
-  obj: {},
-  arr: []
+  firstLevel: {
+    key1: 'test1',
+    secondLevel: {
+      key2: 'test2',
+      thirdLevel: {
+        key3: 'test3',
+        numberArray: [1, 2, 3],
+      }
+    }
+  },
+  arr: ['A', 'B'],
+  str: 'Default string',
+  num: -2
 })
 
 new Vue({
